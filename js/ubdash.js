6bcddf96d3a88f9b628264bd4fbadc8fe7b8b9c4
Jim Mathies <jmathies@mozilla.com>
2023-01-24 12:02:18 -0500

    initial commit
    wip

---
+++ js/ubdash.js
@@ -0,0 +1,234 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*
+  - Settings support and api keys
+
+*/
+ 
+$(document).ready(function () {
+  loadConfig();
+});
+
+function loadConfig() {
+  $.getJSON("config.json", function (configData) {
+    loadPage(configData);
+  }).fail(function () {
+    console.log("getJSON call failed for some reason.")
+  });
+}
+
+function prepPage() {
+  $("#report").empty();
+  //checkConfig();
+}
+
+var ConfigData = {};
+
+function loadPage(configData)
+{
+  ConfigData = configData.config;
+  updateDomains();
+
+  $("#errors").empty();
+
+  prepPage();
+  prepData();
+
+  let url = "https://bugzilla.mozilla.org/rest/bug?";
+
+  // open bugs - 
+  let query = "f1=reporter&o1=equals&v1=update-bot%40bmo.tld&classification=Client%20Software&classification=Developer%20Infrastructure&classification=Components&classification=Server%20Software&classification=Other&resolution=---&&include_fields=id,summary,assigned_to";
+  retrieveInfoFor(url + query, 'open');
+
+  // fixed
+  query = "resolution=FIXED&f1=reporter&chfield=cf_last_resolved&v1=update-bot%40bmo.tld&classification=Client%20Software&classification=Developer%20Infrastructure&classification=Components&classification=Server%20Software&classification=Other&o1=equals&&include_fields=id,summary,assigned_to";
+  retrieveInfoFor(url + query, 'closed');
+
+}
+
+var LastErrorText = "";
+function errorMsg(text) {
+  if (LastErrorText == text)
+    return;
+  $("#errors").append(text);
+  LastErrorText = text;
+}
+
+function retrieveInfoFor(url, userQuery) {
+  $.ajax({
+    url: url,
+    success: function (data) {
+      processListFor(userQuery, data);
+    }
+  })
+  .error(function(jqXHR, textStatus, errorThrown) {
+    console.log("status:", textStatus);
+    console.log("error thrown:", errorThrown);
+    console.log("response text:", jqXHR.responseText);
+    try {
+      let info = JSON.parse(jqXHR.responseText);
+      let text = info.message ? info.message : errorThrown;
+      errorMsg(text);
+      return;
+    } catch(e) {
+    }
+    errorMsg(errorThrown);
+  });
+}
+
+var RegExpSummaryPattern = new RegExp('Update (.*) to new version (.*) from (.*)');
+function parseBugSummary(bugid, summary, assignee) {
+  // 1807473
+  // Update libjxl to new version 5853ad97044c3b9da46d10b611e66063b1297cc5 from 2022-12-22 12:47:29
+
+  let results = RegExpSummaryPattern.exec(summary);
+  if (results == null) {
+    console.log('Error parsing bug summary:');
+    console.log(summary);
+    return null;
+  }
+
+  return {
+    'rev': results[2],
+    'date': new Date(results[3]),
+    'lib': results[1],
+    'id': bugid.toString(),
+    'assignee': assignee
+    };
+}
+
+function prepEntryHeader(category, type) {
+  let header = 
+    "<div class='listhdr-date'>Date</div>" + 
+    "<div class='listhdr-library'>Library</div>" + 
+    "<div class='listhdr-bugid'>Bug</div>" + 
+    "<div class='listhdr-status'>Status</div>" +
+    "<div class='listhdr-assignee'>Owner</div>";
+
+  let id = "list-" + type + category;
+  let subid = "sublist-" + type + category;
+
+  let body = 
+  "<div class='list-container' id='" + id + "'>" +
+  "<div class='sublist-title'>" + category + "</div>" +
+  "<div class='sublist-library'>" +
+  "<div class='sublist-items' id='" + subid + "'></div>" +
+  "</div></div>";
+
+  $("#report-" + type).append(body);
+  $("#" + subid).append(header);
+}
+
+function prepEntry(elId, lib, dt, bugid, status, assignee) {
+  const options = { dateStyle: 'medium' };
+  let dateStr = dt.toLocaleDateString(undefined, options);
+
+  //let tabTarget = ConfigData.targetnew ? "nidetails" : "_blank";
+  let tabTarget = '_blank';
+  let bugUrl = ConfigData.bugzilla_link_url.replace('{id}', bugid);
+  let bugLink = "<a target='" + tabTarget + "' href='" + bugUrl + "'>" + bugid + "</a>";
+
+  let entry = 
+    "<div class='listitem-date'>" + dateStr + "</div>" + 
+    "<div class='listitem-library'>" + lib + "</div>" + 
+    "<div class='listitem-bugid'>" + bugLink + "</div>" + 
+    "<div class='listitem-status'>" + status + "</div>" +
+    "<div class='listitem-assignee'>" + assignee + "</div>";
+
+  $('#' + elId).append(entry);
+}
+
+var OpenBugList = [];
+var ClosedBugList = [];
+
+function prepData() {
+  for (const [key, value] of Object.entries(ConfigData.categories)) {
+    let category = key.toString();
+    OpenBugList[category] = {
+      'listId': "list-open" + category,
+      'sublistId': "sublist-open" + category,
+      'list': []
+    };
+    ClosedBugList[category] = {
+      'listId': "list-closed" + category,
+      'sublistId': "sublist-closed" + category,
+      'list': []
+    };
+  }
+}
+
+function getList(type) {
+  switch(type) {
+    case 'open':
+      return OpenBugList;
+    break;
+    case 'closed':
+      return ClosedBugList;
+    break;
+  }
+}
+
+function processListFor(type, data) {
+  let list = getList(type);
+  data.bugs.forEach(function (bug) {
+    // date, lib, rev, id 
+    let res = parseBugSummary(bug.id, bug.summary, bug.assigned_to);
+    if (res == null) {
+      return;
+    }
+
+    let found = false;
+    for (const [key, value] of Object.entries(ConfigData.categories)) {
+      if (value.includes(res.lib)) {
+        list[key.toString()].list.push(res);
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      list['Misc'].list.push(res);
+    }    
+  });
+
+  // Prep our html tables based on the incoming data
+  for (const [key, value] of Object.entries(list)) {
+    if (value.list.length > 0) {
+      prepEntryHeader(key, type);
+    }
+  }
+
+  displayListFor(type);
+}
+
+function displayListFor(type)
+{
+  let list = getList(type);
+
+  // Categories
+  for (const [key, value] of Object.entries(list)) {
+    // sort by date
+    value.list.sort(sortDateAsc);
+    value.list.forEach(function (bug) {
+      prepEntry(value.sublistId, bug.lib, bug.date, bug.id, type, bug.assignee);
+      let el = document.getElementById(value.listId);
+      el.style.visibility = 'visible';
+    });
+  }
+}
+
+function settingsUpdated() {
+  checkConfig();
+  refreshList(null);
+}
+
+function checkConfig() {
+  if (NeedInfoConfig.api_key.length == 0) {
+    document.getElementById('alert-icon').style.visibility = 'visible';
+  } else {
+    document.getElementById('alert-icon').style.visibility = 'hidden';
+  }
+}
---
+++ js/utils.js
@@ -0,0 +1,262 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+function replaceUrlParam(url, paramName, paramValue) {
+  if (paramValue == null) {
+    paramValue = '';
+  }
+  var pattern = new RegExp('\\b(' + paramName + '=).*?(&|#|$)');
+  if (url.search(pattern) >= 0) {
+    return url.replace(pattern, '$1' + paramValue + '$2');
+  }
+  url = url.replace(/[?#]$/, '');
+  return url + (url.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue;
+}
+
+function getTeam() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('team');
+}
+
+function getUserId() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('userid');
+}
+
+function getUserQuery() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('userquery');
+}
+
+function updateDomains() {
+  // If requested via the json config file, point all queries at
+  // a bugzilla test instance. 
+  let domain = ConfigData.bugzilla_domain;
+  if (ConfigData.use_test_domain) {
+    domain = ConfigData.bugzilla_test_domain;
+  }
+  ConfigData.bugzilla_search_url =
+    ConfigData.bugzilla_search_url.replace('{domain}', domain);
+  ConfigData.bugzilla_put_url =
+    ConfigData.bugzilla_put_url.replace('{domain}', domain);
+  ConfigData.bugzilla_link_url =
+    ConfigData.bugzilla_link_url.replace('{domain}', domain);
+  ConfigData.bugzilla_user_url =
+    ConfigData.bugzilla_user_url.replace('{domain}', domain);
+
+  console.log("Bugzilla target:", domain);
+}
+
+// generate random integer in the given range
+function randomNumber(min, max) { 
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function restToQueryUrl(url) {
+  // '/rest/bug' | '/buglist.cgi'
+  return url.replace('/rest/bug', '/buglist.cgi');
+}
+
+function trimAddress(account) {
+  if (account == undefined) {
+    // Unassigned
+    account = '';
+  }
+
+  for (const [key, value] of Object.entries(NeedInfoConfig.developers)) {
+    if (value == account) {
+      account = key;
+      break;
+    }
+  }
+
+  account = account.replace('nobody@mozilla.org', 'nobody');
+
+  // Todo: get the alias from bugzilla
+
+  // aryx.bugmail@gmx-topmail.de
+  account = account.replace('aryx.bugmail@gmx-topmail.de', 'Aryx');
+  // ryanvm@gmail.com
+  account = account.replace('ryanvm@gmail.com', 'RyanVM');
+  // nagbot
+  account = account.replace('release-mgmt-account-bot@mozilla.tld', 'nag-bot');
+  // updatebot
+  account = account.replace('update-bot@bmo.tld', 'update-bot');
+
+  account = account.replace('@mozilla.org', '@moz');
+  account = account.replace('@mozilla.com', '@moz');
+  return account;
+}
+
+function getFromStorage(keyname) {
+  let value = sessionStorage.getItem(keyname);
+  if (value == null || !value.length) {
+    //console.log('session storage value for ', keyname, ' does not exist.');
+    value = localStorage.getItem(keyname);
+    if (value == null || !value.length) {
+      //console.log('persistent storage value for ', keyname, ' does not exist.');
+    } else {
+      //console.log('persistent storage value for ', keyname, ':', value);
+    }
+  } else {
+    //console.log('session storage value for ', keyname, ':', value);
+  }
+  return value;
+}
+
+function clearStorage(keyname) {
+  localStorage.removeItem(keyname);
+  sessionStorage.removeItem(keyname);
+}
+
+function saveDefaultSortSettings(type, currentOrder) {
+  if (NeedInfoConfig.saveoptions) {
+    localStorage.setItem("sort", type); // string
+    localStorage.setItem("sortorder", currentOrder); // boolean
+  }
+}
+
+function getDefaultSortSettings() {
+  return { 'sort': getFromStorage('sort'), 'order': getFromStorage('sortorder') };
+}
+
+function loadSettingsInternal() {
+  let api_key = getFromStorage("api-key");
+
+  NeedInfoConfig.api_key = (api_key == null) ? "" : api_key;
+  NeedInfoConfig.ignoremyni = getFromStorage("ignoremyni") == (null || 'false') ? false : true;
+  NeedInfoConfig.saveoptions = getFromStorage("save") == (null || 'false') ? false : true;
+  NeedInfoConfig.targetnew = getFromStorage("target") == (null || 'false') ? false : true;
+
+  console.log('storage key:', NeedInfoConfig.api_key);
+  console.log('ignore:', NeedInfoConfig.ignoremyni);
+  console.log('persist:', NeedInfoConfig.saveoptions);
+  console.log('targets:', NeedInfoConfig.targetnew);
+}
+
+function openSettings() {
+  if (NeedInfoConfig.api_key.length) {
+    document.getElementById("api-key").value = NeedInfoConfig.api_key;
+  }
+  document.getElementById("option-ignoremyni").checked = NeedInfoConfig.ignoremyni;
+  document.getElementById("option-save").checked = NeedInfoConfig.saveoptions;
+  document.getElementById("option-targets").checked = NeedInfoConfig.targetnew;
+
+  document.getElementById("popupForm").style.display = "block";
+}
+
+function closeSettings() {
+  document.getElementById("popupForm").style.display = "none";
+}
+
+function saveSettings(e) {
+  e.preventDefault();
+
+  let x = $("form").serializeArray();
+  let values = {};
+  $.each(x, function (i, field) {
+    values[field.name] = field.value;
+  });
+
+  // 'remember my settings' checkbox
+  let usePersistent = JSON.stringify(values).includes("save");
+  console.log('use persistent storage:', usePersistent);
+
+  // API key
+  let old_api_key = "";
+  let key = getFromStorage("api-key");
+  if (key != null) {
+    old_api_key = key;
+  }
+
+  let storage = usePersistent ? localStorage : sessionStorage;
+
+  clearStorage("api-key");
+  storage.setItem("api-key", values.key);
+
+  clearStorage("ignoremyni");
+  storage.setItem("ignoremyni", values.ignoremyni == 'on' ? true : false);
+
+  clearStorage("save");
+  storage.setItem("save", values.save == 'on' ? true : false);
+
+  clearStorage("target");
+  storage.setItem("target", values.target == 'on' ? true : false);
+
+  closeSettings();
+  loadSettingsInternal();
+  settingsUpdated();
+}
+
+/* sorting utilities */
+
+// sorting:
+// If the result is negative, a is sorted before b.
+// If the result is positive, b is sorted before a.
+// If the result is 0, no changes are done with the sort order of the two values.
+
+function sortDateAsc(a, b) {
+  return a.date < b.date;
+}
+
+function sortDateDesc(a, b) {
+  return a.date > b.date;
+}
+
+function sortBugIdAsc(a, b) {
+  return a.bugid < b.bugid;
+}
+
+function sortBugIdDesc(a, b) {
+  return a.bugid > b.bugid;
+}
+
+// severity - S1 - S4, enhancement, trivial, minor, normal, major, critical, blocker, N/A, --
+var sVals = {
+  '--': 0,
+  'S1': 1,
+  'S2': 2,
+  'S3': 3,
+  'S4': 4,
+  'blocker': 1,
+  'critical': 1,
+  'major': 2,
+  'normal': 3,
+  'minor': 4,
+  'trivial': 4,
+  'enhancement': 4,
+  'N/A': 4,
+};
+
+function sortSeverityAsc(a, b) {
+  return sVals[a.severity] >= sVals[b.severity];
+}
+
+function sortSeverityDesc(a, b) {
+  return sVals[a.severity] < sVals[b.severity];
+}
+
+var pVals = {
+  '--': 0,
+  'P1': 1,
+  'P2': 2,
+  'P3': 3,
+  'P4': 4,
+  'P5': 5,
+};
+
+function sortPriorityAsc(a, b) {
+  return pVals[a.priority] >= pVals[b.priority];
+}
+
+function sortPriorityDesc(a, b) {
+  return pVals[a.priority] < pVals[b.priority];
+}
+
+
